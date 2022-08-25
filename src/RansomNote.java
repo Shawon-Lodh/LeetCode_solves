3dcc4d125c20932687fe0880fdb3d5dd9eb09db1
Shawon-Lodh <50014582+Shawon-Lodh@users.noreply.github.com>
2022-08-25 12:47:08 +0600

    add solution for - Ransom Note - problem no - 383

---
+++ src/RansomNote.java
@@ -0,0 +1,35 @@
+public class RansomNote {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        boolean status = false;
+        StringBuilder magazineStr = new StringBuilder().append(magazine);
+        StringBuilder ransomNoteStr = new StringBuilder().append(ransomNote);
+        int matchedIndex = -1;
+
+        for(int i = 0; i < ransomNoteStr.length(); i++)
+        {
+            matchedIndex = magazineStr.indexOf(String.valueOf(ransomNoteStr.charAt(i)));
+            if(matchedIndex == -1){
+                status = false;
+                break;
+            }else{
+                status = true;
+                magazineStr.deleteCharAt(matchedIndex);
+            }
+        }
+        return status;
+    }
+
+    public static void main(String[] args) {
+
+        RansomNote solutionObj = new RansomNote();
+
+        String input1 = "ab";
+        String input2 = "bb";
+
+        boolean output = solutionObj.canConstruct(input1,input2);
+
+        System.out.println("Output is : " + output);
+
+    }
+}
