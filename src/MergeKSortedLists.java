8e9baeb97025c4d153b0e62796395c93622b39dc
Shawon-Lodh <50014582+Shawon-Lodh@users.noreply.github.com>
2022-09-13 18:08:22 +0600

    update solution for - Merge k Sorted Lists - 23

--- src/MergeKSortedLists.java
+++ src/MergeKSortedLists.java
@@ -1,33 +1,24 @@
 import java.util.PriorityQueue;
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 public class MergeKSortedLists {
-    public ListNode mergeKLists(ListNode[] lists) {
+    public LinkedListNodeClass.ListNode mergeKLists(LinkedListNodeClass.ListNode[] lists) {
 
         PriorityQueue<Integer> getMergedListVal = getMergedList(lists);
 
-        ListNode result = new ListNode(-1);
-        ListNode current = result;
+        LinkedListNodeClass.ListNode result = new LinkedListNodeClass.ListNode(-1);
+        LinkedListNodeClass.ListNode current = result;
 
         while (!getMergedListVal.isEmpty()){
-            current.next = new ListNode(getMergedListVal.poll());
+            current.next = new LinkedListNodeClass.ListNode(getMergedListVal.poll());
             current = current.next;
         }
 
         return result.next;
     }
 
-    public PriorityQueue<Integer> getMergedList(ListNode[] lists) {
+    public PriorityQueue<Integer> getMergedList(LinkedListNodeClass.ListNode[] lists) {
         PriorityQueue<Integer> result = new PriorityQueue<>();
         for(int i = 0; i < lists.length; i++){
-            ListNode temp = lists[i];
+            LinkedListNodeClass.ListNode temp = lists[i];
             System.out.println();
             while(temp!=null){
                 result.add(temp.val);
@@ -37,8 +28,8 @@ public class MergeKSortedLists {
         return result;
     }
 
-    public void printLinkedList(ListNode result , String msg){
-        ListNode resultTemp = result;
+    public void printLinkedList(LinkedListNodeClass.ListNode result , String msg){
+        LinkedListNodeClass.ListNode resultTemp = result;
         System.out.print(msg);
         while(resultTemp!=null){
             System.out.print(resultTemp.val+" => ");
@@ -50,17 +41,17 @@ public class MergeKSortedLists {
 
         MergeKSortedLists solutionObj = new MergeKSortedLists();
 
-        ListNode[] input = new ListNode[3];
+        LinkedListNodeClass.ListNode[] input = new LinkedListNodeClass.ListNode[3];
 
 //        for(int i = 0 ;i < inputLists.length ; i++){
-//            ListNode temp = new ListNode(new Random().nextInt(100),new ListNode(new Random().nextInt(100),new ListNode(new Random().nextInt(100))));
+//            LinkedListNodeClass.ListNode temp = new LinkedListNodeClass.ListNode(new Random().nextInt(100),new LinkedListNodeClass.ListNode(new Random().nextInt(100),new LinkedListNodeClass.ListNode(new Random().nextInt(100))));
 //            inputLists[i] = temp;
 //        }
 
 
-        input[0] = new ListNode(1,new ListNode(4,new ListNode(5)));
-        input[1] = new ListNode(1,new ListNode(3,new ListNode(4)));
-        input[2] = new ListNode(2,new ListNode(6));
+        input[0] = new LinkedListNodeClass.ListNode(1,new LinkedListNodeClass.ListNode(4,new LinkedListNodeClass.ListNode(5)));
+        input[1] = new LinkedListNodeClass.ListNode(1,new LinkedListNodeClass.ListNode(3,new LinkedListNodeClass.ListNode(4)));
+        input[2] = new LinkedListNodeClass.ListNode(2,new LinkedListNodeClass.ListNode(6));
 
 //        System.out.print("Input Linked List is :");
 //        for(int i = 0 ;i < input.length ; i++){
@@ -68,7 +59,7 @@ public class MergeKSortedLists {
 //            System.out.println();
 //        }
 
-        ListNode output = solutionObj.mergeKLists(input);
+        LinkedListNodeClass.ListNode output = solutionObj.mergeKLists(input);
 
         solutionObj.printLinkedList(output,"Result linked list is : ");
 
